0468c9947c2c1b3a7c2343a5f71596c04303ea7c
Jonathon Mangan <jtmangac@gmail.com>
2023-04-08 11:23:34 -0400

    Added the spring/java app to the project backend

---
+++ restaurant_back_end/src/test/java/com/example/demo/RestaurantBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
